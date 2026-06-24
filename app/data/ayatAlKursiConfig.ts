f91b4b403621b56fc9b7fec81fa7959b4c4893fc
erfan <erfan.mirasadi@gmail.com>
2026-06-24 16:19:49 +0300

    adjust s2Gap in ayatAlKursiConfig to match alak96Config alignment

--- app/data/ayatAlKursiConfig.ts
+++ app/data/ayatAlKursiConfig.ts
@@ -190,7 +190,7 @@ export const AYAT_AL_KURSI_CONFIG: SurahLayoutConfig<AlakLayoutParams> = {
     groupGap: 0.025,
     groupPad: 0.012,
     groupPadBottom: 0.012,
-    s2Gap: 0.06, // reduced horizontal gap
+    s2Gap: 0.02, // matching alak96Config.ts
     s2VerticalRowGap: 0.046, // the vertical gap between rows in the same group!
     smallBoxH2: 0.085, // height of each individual verse capsule
     middleExtraGap: 0.007,
